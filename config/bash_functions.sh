d9d1e0b0a9f7de7ce505b2f99605c33088fc2df7
Seb Norris <sebastian@automationlogic.com>
2018-07-11 14:23:24 +0100

    shorten function definitions

--- config/bash_functions.sh
+++ config/bash_functions.sh
@@ -2,20 +2,20 @@
 
 # Some functions that come in handy every now and again
 
-# count number of times a phrase appears in each file of a directory
-numgrep(){
+# numgrep: count number of times a phrase appears in each file of a directory
+ng(){
     grep --count -ir $1 . | grep -v :0
 }
 
 # for renaming terminal windows
-function title {
+title() {
     echo -ne "\033]0;"$*"\007"
 }
 
 # clear screen with new lines
 # useful when you need to see where one long output ends
 # and another begins
-function cl {
+cl() {
     count=0;
     while [ $count -lt 50 ]; do
         echo ""
