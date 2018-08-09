fe9121a5e0935aabdfb3524918f6cfb80cdf7969
Seb Norris <sebastian@automationlogic.com>
2018-08-09 14:42:08 +0100

    added bash profile

--- python_scripts/slackgrid.py
+++ python_scripts/slackgrid.py
@@ -1,10 +1,9 @@
-# 
-# Takes an image and splits it into 9 different slack
-# emojis of appropriate dimensions
-#
-
+"""
+Takes an image and splits it into 9 different slack emojis of appropriate dimensions
+"""
 import Image
-import os, sys
+import os
+import sys
 
 # open image
 input_picture_name = sys.argv[1]
@@ -15,20 +14,18 @@ resized_image = input_picture.resize(new_size, Image.ANTIALIAS)
 
 # create 9 images that are 128x128
 
-resized_image.crop((0, 0, 50, 50)).save('output.png')
-
 area_dictionary = {
-    "top_left"       : (0,0,128,128),
-    "top_middle"     : (128,0,256,128),
-    "top_right"      : (256,0,384,128),
+    "top_left"       : (0, 0, 128, 128),
+    "top_middle"     : (128, 0, 256, 128),
+    "top_right"      : (256, 0, 384, 128),
 
-    "middle_left"   : (0,128,128,256),
-    "middle_middle" : (128,128,256,256),
-    "middle_right"  : (256,128,384,256),
+    "middle_left"   : (0, 128, 128, 256),
+    "middle_middle" : (128, 128, 256, 256),
+    "middle_right"  : (256, 128, 384, 256),
 
-    "bottom_left"   : (0,256,128,384),
-    "bottom_middle" : (128,256,256,384),
-    "bottom_right"  : (256,256,384,384)
+    "bottom_left"   : (0, 256, 128, 384),
+    "bottom_middle" : (128, 256, 256, 384),
+    "bottom_right"  : (256, 256, 384, 384)
 }
 
 picture_prefix = input_picture_name[0:-4]
